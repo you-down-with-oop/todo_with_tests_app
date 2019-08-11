6f617c637623f429af5e14c1e06af9911ca34ee0
Peter Jang <peter.x.jang@gmail.com>
2019-08-11 11:34:10 -0500

    Run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_11_151926) do
+ActiveRecord::Schema.define(version: 2019_08_11_163030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -25,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_08_11_151926) do
     t.boolean "favorite"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "list_id"
   end
 
 end
